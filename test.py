f975640598dab7356019819c8daeb25a1548ae37
Hanseok <70805713+Hxnss@users.noreply.github.com>
2024-11-13 14:41:40 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-import streamlit as st
+simport streamlit as st
 
 Judul = st.title("Hello World!", anchor='right')
 Sub = st.subheader("The World is so beautifull.")
 st.header("World is so complex.")
 st.text("Earth is Our World. So if the earth not match with our condition anymore, we would die.")
 st.markdown("[YouTube](https://www.youtube.com/@gurugembul)")
+st.image(image, width = 800)
